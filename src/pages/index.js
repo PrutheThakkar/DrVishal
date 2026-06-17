cd49b91cda8ea7003ca64fe41b6fb3077c1f4681
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-06-17 14:53:33 +0530

    vishal-deedback done

--- src/pages/index.js
+++ src/pages/index.js
@@ -466,6 +466,7 @@ export default function IndexPage() {
                     setActiveProductIndex(index)
                   }}
                 />
+                
               ))}
             </div>
           </div>
